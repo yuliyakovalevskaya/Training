f34feff674becfe5caca73b82a30f69b55b60db5
Yuliya Kovalevskaya <y.i.kovalevskaya@gmail.com>
2022-01-18 16:59:25 +0300

    The project is created for training transfer via goto.

---
+++ Transfer (goto)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine("Выведем на экран последовательно числа от 0 до 10");
+        Label:
+            Console.WriteLine("a=" + a);
+            a = a + 1;
+            if (a <= 10)
+                goto Label;
+            Console.ReadKey();
+        }
+           
+    }
+}
