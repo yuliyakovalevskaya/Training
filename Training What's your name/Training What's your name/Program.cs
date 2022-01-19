d26ea17f195433e5fcce0222a056a4e3d1f8bbb4
Yuliya Kovalevskaya <y.i.kovalevskaya@gmail.com>
2022-01-19 13:12:36 +0300

    Creating a project.

---
+++ Training What's your name/Training What's your name/Program.cs
@@ -0,0 +1,21 @@
+﻿// Creating a project.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication7
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Как тебя зовут?");
+            name = Console.ReadLine();
+            Console.WriteLine("Привет," + name);
+            Console.ReadKey();
+        }
+    }
+}
+
